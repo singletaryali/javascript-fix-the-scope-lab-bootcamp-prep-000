3c78e8bc21122da5988477bd0320ecef1329f95e
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-29 14:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-function funkyFunction () {
+function theFunk () {
   return "FUNKY!"
 }
 
