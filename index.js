809ad1563a175064470bdc0dd7c4cd370747a4c3
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-29 14:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal = 'cat';
   
-  console.log ('${animal}')
+  console.log ('${animal}');
 }
 
 function add2(n) {
